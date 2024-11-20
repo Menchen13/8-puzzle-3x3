cf4cab98ede10c018aec22f81025ea4abfba3c55
Nik Hildebrandt <nik.hildebrandt@proton.me>
2024-11-20 20:42:54 +0100

    added command line arguments using CLI11.hpp

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A project for my AI lecture at the Leibniz-fh.
 
 ## Specification
 I have choosen the specific implementation provided by this [site](https://www.8puzzle.com/8_puzzle_problem.html).
-I will therefore only consider these two goal states:
+I will therefore consider these two goal states:
 ![Goal States](image/Goal_States.png)
 
 # TODO
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "solver.h"
 #include <algorithm>
 #include <random>
+#include <CLI11.hpp>
 
 //returns true if the array contains a legal field
 //needed for player input validation
@@ -12,20 +13,74 @@ bool legal_field(std::array<int, 9> field);
 //used if the player doesnt provide a starting field
 std::array<int, 9 > random_field();
 
-int main(){
-    // auto field = random_field();
-    // Node * test = new Node(field);
-    // test->print_field();
-    // solve(test, A_star{});
+//exit codes
+//-1 bad starting field
+//-2 bad search strategie flags
+int main(int argc, char* argv[]){
+    //comandline argument parsing using CLI11
+    CLI::App app{"A Programm to solve the 8-puzzle problem for my lecture", "8-puzzle"};
 
-    std::array<int,9> start = {2,8,3,1,6,4,7,0,5};
-    if(!legal_field(start)){
-        std::cout << "Illegal starting field detected!!!\nExiting" << std::endl;
+    bool A = false;
+    bool G = false;
+    app.add_flag("-a", A, "Use A* search strategie(default)");
+    app.add_flag("-g", G, "Use greedy search strategie");
+
+    std::string starting_field;
+    app.add_option("starting_field", starting_field, "Optional input: A field for the programm to start from. Example: 324015786");
+
+    CLI11_PARSE(app, argc, argv);
+
+    //set starting field based on CLI parameters
+    std::array<int, 9> field;
+
+    //if user provided a starting field
+    //validate input and load it into the start array
+    if(!starting_field.empty()){
+        //check if lenght is correct before reading into array
+        if(starting_field.length() != 9){
+            std::cout << "Bad starting field! Use -h for more info!" << std::endl;
+            return -1;
+        }
+
+        //read user input into array
+        for (int i = 0; i < 9; i++)
+        {
+            // convert strings to numeric values
+            field[i] = (starting_field.at(i) - '0');
+        }
+
+        //check if provided field is legal for puzzle
+        if(!legal_field(field)){
+            std::cout << "Bad starting field! Use -h for more info!" << std::endl;
+            return -1;
+        }
+    }
+    //if user did not provide a starting field
+    //generate a random one, no validation needed in this case
+    else{
+        field = random_field();
+    }
+
+    //create start Node with field
+    Node* start = new Node(field);
+
+    //start the solver based on search strategie flags
+
+    //if both flags are set exit with error code -2
+    if(A && G){
+        std::cout << "Bad search stategie flags! Use at most one flag!" << std::endl;
         return -2;
     }
-    Node* startNode = new Node(start);
-    solve(startNode, A_star{});
-
+    //if -g is used start solve with greedy
+    if(G){
+        std::cout << "Solving with greedy! This might brick your PC if you dont have a lot of RAM!" << std::endl;
+        solve(start, Greedy{});
+    }
+    //else start solve with A*
+    //this includes the default case, where no flag was parsed
+    else{
+        solve(start, A_star{});
+    }
 }
 
 bool legal_field(std::array<int, 9> field){
